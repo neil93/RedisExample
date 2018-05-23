dacaf63074043eb5fbc9a06ba7b629dcbecae7f6
Neil.Tsai <neil.tsai@xuenn.com>
2018-05-23 18:15:14 +0800

    Redis範例

---
+++ RedisTest/RedisConnectionFactory.cs
@@ -0,0 +1,33 @@
+﻿using StackExchange.Redis;
+
+namespace RedisTest
+{
+    public class RedisConnectionFactory
+    {
+        private readonly object _obj = new object();
+
+        private volatile ConnectionMultiplexer _redis;
+
+        public ConnectionMultiplexer GetRedisInstance
+        {
+            get
+            {
+                if (_redis==null)
+                {
+                    lock (_obj)
+                    {
+                        if (_redis==null || !_redis.IsConnected || !_redis.GetDatabase(0).IsConnected(new RedisKey()))
+                        {
+                            _redis = ConnectionMultiplexer.Connect("172.16.45.34:9417");
+                        }
+                    }
+                    
+                }
+                return _redis;
+            }
+            
+        }
+
+
+    }
+}
